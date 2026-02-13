737606b5288d1d497000d321839af8522fa55688
Nitinsingh200 <nitinsinghna12345@gamil.com>
2026-02-13 11:24:18 +0530

    first commit

---
+++ frontend/src/redux/companySlice.js
@@ -0,0 +1,45 @@
+// import { createSlice } from "@reduxjs/toolkit";
+
+// const companySlice = createSlice({
+//     name:"company",
+//     initialState:{
+//         singleCompany:null,
+//         companies:[]
+     
+//     },
+//     reducers:{
+//         // actions
+//         setSingleCompany:(state,action) => {
+//             state.singleCompany = action.payload;
+//         },
+//         setCompnaies:(state,action) => {
+//             state.companies = action.payload;
+//         },
+      
+//     }
+// });
+// export const {setSingleCompany,setCompnaies} = companySlice.actions;
+// export default companySlice.reducer;
+import { createSlice } from "@reduxjs/toolkit";
+
+const companySlice = createSlice({
+  name: "company",
+  initialState: {
+    singleCompany: null,
+    companies: [],
+    searchCompanyByText:"",
+  },
+  reducers: {
+    setSingleCompany: (state, action) => {
+      state.singleCompany = action.payload;
+    },
+    setCompanies: (state, action) => {   
+      state.companies = action.payload;
+    },
+    setSearchCompanyByText:(state,action) => {
+      state.searchCompanyByText = action.payload;
+  }
+  },
+});
+export const {setSingleCompany, setCompanies,setSearchCompanyByText} = companySlice.actions;
+export default companySlice.reducer;
